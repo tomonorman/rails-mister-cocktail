104cfc6befa7974ef3e23a543b89f733bfeba357
Tomo Norman <tomo.l.norman@gmail.com>
2020-11-05 12:05:32 +0900

    ingredients added to db

---
+++ app/models/dose.rb
@@ -0,0 +1,7 @@
+class Dose < ApplicationRecord
+  validates :description, presence: true
+  validates_uniqueness_of :cocktail_id, scope: :ingredient_id
+
+  belongs_to :cocktail
+  belongs_to :ingredient
+end
