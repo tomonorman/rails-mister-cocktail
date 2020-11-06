7eaee42edb91e384f370883b1eb8ab1fe77e1c16
Tomo Norman <tomo.l.norman@gmail.com>
2020-11-06 12:09:30 +0900

    active stprage and cloudingary added

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
 
-  resources :cocktails, only: [:index, :show, :new, :create] do
+  root "cocktails#index"
+  resources :cocktails, only: [:show, :new, :create] do
     resources :doses, only: [:new, :create]
   end
   resources :doses, only: [:destroy]
